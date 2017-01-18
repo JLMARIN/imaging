04a0fb76d77f9cc7eddfd6c699f50fa4703c044b
Jorge Marin <jlmarinmarcano@gmail.com>
2017-01-18 15:39:15 -0700

    updated stream_capture to use xml parser and updated captureloopsweep

--- codes/captureloopsweep_1.py
+++ codes/captureloopsweep_1.py
@@ -4,7 +4,7 @@ import sys
 import time
 import os
 import subprocess
-import ConfigParser
+import xml.etree.ElementTree as ET
 from customTimer import RepeatedTimer
 
 # define resolution tuples in format (width,height)
@@ -19,8 +19,25 @@ res_1920_1080 = (1920, 1080)
 res_2048_1536 = (2048, 1536)
 res_2592_1944 = (2592, 1944)
 
+dev_id = 0
+
 # define global name 'frame' before using
 frame = 0
+
+# define array of counter values for camera settings update
+inc = 10
+pic_interval = [inc,
+                2 * inc,
+                3 * inc,
+                4 * inc,
+                5 * inc,
+                6 * inc,
+                7 * inc,
+                8 * inc,
+                9 * inc,
+                10 * inc]
+
+video_duration = 30
 
 
 class Tee:
@@ -33,20 +50,6 @@ class Tee:
         self.out2.write(*args, **kwargs)
 
 
-def ConfigSectionMap(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                print "skip: %s" % option
-        except:
-            print "exception on %s!" % option
-            dict1[option] = None
-    return dict1
-
-
 def setup_logger():
     timestr = time.strftime("%Y%m%d-%H%M%S")
     file_name = os.path.splitext(sys.argv[0])[0]
@@ -56,6 +59,8 @@ def setup_logger():
 def set_resolution(*args):
     vc.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, args[0][0])   # set frame width in pixels
     vc.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, args[0][1])  # set frame height in pixels
+    print "camera settings update:"
+    print "    @resolution=" + str(args[0][0]) + 'x' + str(args[0][1])
 
 
 def setup(device, res, setFlag, brt, exp_auto, exp_abs):
@@ -91,21 +96,11 @@ capture_frame.name_count = len(glob.glob('pics/*.jpg'))
 
 if __name__ == '__main__':
 
-    # read config file
-    Config = ConfigParser.ConfigParser()
-    Config.read("config.ini")
+    # read config xml file
+    tree = ET.parse("config.xml")
+    config = tree.getroot()
 
-    dev_id = int(ConfigSectionMap("TargetCamera")['dev_id'])
-
-    setup(dev_id, res_2048_1536, False, 0, 0, 0)
-    setup(dev_id, res_2592_1944, True, 15, 1, 20)
-    setup(dev_id, res_2592_1944, True, 10, 1, 20)
-    setup(dev_id, res_2592_1944, True, 15, 1, 10)
-    setup(dev_id, res_2592_1944, True, 10, 1, 10)
-    setup(dev_id, res_2048_1536, True, 15, 1, 20)
-    setup(dev_id, res_2048_1536, True, 10, 1, 20)
-    setup(dev_id, res_2048_1536, True, 15, 1, 10)
-    setup(dev_id, res_2048_1536, True, 10, 1, 10)
+    dev_id = int(config.find('CameraSettings/dev_id').text)
 
     vc = cv2.VideoCapture(dev_id)
 
@@ -117,28 +112,69 @@ if __name__ == '__main__':
         print "> found " + str(capture_frame.name_count) + " image files in pics folder"
 
         # =============> video capture section
-        setup(dev_id, res_2592_1944, False, 0, 0, 0)
+
+        # Define the codec and create VideoWriter object
+        fourcc = cv2.cv.CV_FOURCC('X', 'V', 'I', 'D')
+        video = cv2.VideoWriter('video.avi', fourcc, 25.0, res_1280_1024)
 
         print "> starting video capture"
-        t_end = time.time() + 240
+
+        setup(dev_id, res_1280_1024, False, 0, 0, 0)
+
+        t_end = time.time() + video_duration
 
         while time.time() < t_end:
+            # read frame
+            retval, frame = vc.read()
+            # write frame
+            video.write(frame)
+
+        print "> video capture finished"
 
         # =============> end of video capture section
 
         print "> starting picture capture"
+
+        setup(dev_id, res_2592_1944, False, 0, 0, 0)
 
         rt = RepeatedTimer(1, capture_frame)
 
         try:
             while True:
                 retval, frame = vc.read()
+                if capture_frame.count == pic_interval[0]:
+                    setup(dev_id, res_2048_1536, False, 0, 0, 0)
+
+                if capture_frame.count == pic_interval[1]:
+                    setup(dev_id, res_2592_1944, True, 10, 1, 20)
+                if capture_frame.count == pic_interval[2]:
+                    setup(dev_id, res_2048_1536, True, 10, 1, 20)
+
+                if capture_frame.count == pic_interval[3]:
+                    setup(dev_id, res_2592_1944, True, 8, 1, 20)
+                if capture_frame.count == pic_interval[4]:
+                    setup(dev_id, res_2048_1536, True, 8, 1, 20)
+
+                if capture_frame.count == pic_interval[5]:
+                    setup(dev_id, res_2592_1944, True, 10, 1, 10)
+                if capture_frame.count == pic_interval[6]:
+                    setup(dev_id, res_2048_1536, True, 10, 1, 10)
+
+                if capture_frame.count == pic_interval[7]:
+                    setup(dev_id, res_2592_1944, True, 8, 1, 10)
+                if capture_frame.count == pic_interval[8]:
+                    setup(dev_id, res_2048_1536, True, 8, 1, 10)
+
+                if capture_frame.count == pic_interval[9]:
+                    print "> picture capture finished"
+                    break
         except KeyboardInterrupt:
             print "loop interrupted!"
 
         rt.stop()
 
         vc.release()
+        video.release()
         cv2.destroyAllWindows()
 
     print "> program exit [" + str(time.strftime("%Y%m%d-%H%M%S")) + "]"
---
+++ codes/tests/write_video.py
@@ -0,0 +1,47 @@
+import cv2
+
+# define resolution tuple in format (width,height). Uncomment the desired resolution
+# resolution = (320, 240)
+# resolution = (640, 480)
+# resolution = (800, 600)
+# resolution = (1024, 768)
+# resolution = (1280, 720)
+resolution = (1280, 1024)
+# resolution = (1600, 1200)
+# resolution = (1920, 1080)
+# resolution = (2048, 1536)
+# resolution = (2592, 1944)
+
+
+def set_resolution(*args):
+
+    vc.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, args[0][0])   # set frame width in pixels
+    vc.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, args[0][1])  # set frame height in pixels
+    print "camera settings update:"
+    print "    @resolution=" + str(args[0][0]) + 'x' + str(args[0][1])
+
+
+vc = cv2.VideoCapture(1)
+
+set_resolution(resolution)
+
+# Define the codec and create VideoWriter object
+fourcc = cv2.cv.CV_FOURCC('X', 'V', 'I', 'D')
+video = cv2.VideoWriter('video.avi', fourcc, 25.0, resolution)
+
+while(vc.isOpened()):
+    ret, frame = vc.read()
+    if ret is True:
+        # write the frame
+        video.write(frame)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+vc.release()
+video.release()
+cv2.destroyAllWindows()
