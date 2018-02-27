68f12d1790e5c964c70d40f51fd8c769c04f168b
Jorge <jlmarinmarcano@gmail.com>
2018-02-27 16:55:10 -0700

    added focus measurement to viewcam

--- focus/measurefocus/tests/viewcam/main.cpp
+++ focus/measurefocus/tests/viewcam/main.cpp
@@ -16,7 +16,40 @@
 #include <libv4l2.h>
 #include <linux/videodev2.h>
 
+#include <math.h>       /* pow */
+
 using namespace cv;
+
+/**
+ * @brief   Focus measure implementation using the 'Variance of laplacian (Pech2000)'.
+ *          Informatio taken from:
+ *          - S. Pertuz et al., Analysis of focus measure operators for shape-from-focus.
+ *            Pattern Recognition, 46(5):1415:1432, 2013
+ * @param   Image to process
+ * @return  Focus measure
+ */
+float fmeasure( const Mat &src )
+{
+    Point anchor( 0, 0 );
+    double delta = 0;
+    int ddepth = -1;
+    Scalar mean, stddev;
+
+    // laplacian filter kernel
+    float kernel[3][3] = { {0.1667, 0.6667, 0.1667} , {0.6667, -3.3333, 0.6667} , {0.1667, 0.6667, 0.1667} };
+
+    Mat ker = Mat( 3, 3, CV_32F, &kernel );
+    Mat dst = Mat( src.size(), src.type() );
+
+    // apply filter to image
+    filter2D( src, dst, ddepth , ker, anchor, delta, BORDER_REPLICATE );
+
+    // calculate standard deviation
+    meanStdDev(dst, mean, stddev);
+
+    // return focus measure as the standard deviation squared
+    return static_cast<float>( pow(stddev.val[0], 2) );
+}
 
 void configureCamera(int fd)
 {
@@ -34,19 +67,27 @@ void configureCamera(int fd)
 
     // set exposure
     ctrl.id = V4L2_CID_EXPOSURE_ABSOLUTE;
-    ctrl.value = 2;
+    ctrl.value = 10;
     v4l2_ioctl(fd, VIDIOC_S_CTRL, &ctrl);
 
     // set trigger
     ctrl.id = V4L2_CID_PRIVACY;
     ctrl.value = 0;
     v4l2_ioctl(fd, VIDIOC_S_CTRL, &ctrl);
+
+    // set frame rate
+    struct v4l2_streamparm setfps;
+    setfps.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    setfps.parm.capture.timeperframe.numerator = 1;
+    setfps.parm.capture.timeperframe.denominator = 10;
+    v4l2_ioctl(fd, VIDIOC_S_PARM, &setfps);
 }
 
 int main ()
 {
     IplImage* pOpenCVImage;
-    IplImage* pColorCVImage;
+    Mat image;
+    float focus;
 
     CvSize ImageSize;
     unsigned char* ImageBuffer = NULL;
@@ -78,8 +119,16 @@ int main ()
         if( ImageBuffer != NULL )
         {
             memcpy( pOpenCVImage->imageData, ImageBuffer, pOpenCVImage->imageSize);
+            
+            image = cvarrToMat(pOpenCVImage);
+            //image = mat_frame(pOpenCVImage);
 
             cvShowImage( (char*)"Camera", pOpenCVImage);
+            //imshow( "Camera", image);
+
+            focus = fmeasure( image );
+            printf("focus = %4.4f\r", focus);
+
             wKey = cvWaitKey(10);
         }
         else
@@ -94,7 +143,7 @@ int main ()
     uninit_device();
     close_device();
 
-    printf("Program ended\n");
+    printf("\nProgram ended\n");
 
     return 0;
 }
